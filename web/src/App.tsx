afc5ced38d68f23df05e240f012490596266ae71
Natasha Chaves <natashachavessilva@gmail.com>
2022-05-02 14:08:15 -0300

    Stage 1
    Configurando ambiente de desenvolvimento, componentes e propriedades, utilizando o  tailwind.css, conceitos de acessibilidades e uso do  popover.

---
+++ web/src/App.tsx
@@ -0,0 +1,7 @@
+import { Widget } from "./components/Widget"
+
+export function App() {
+  return <Widget />
+}//TSX {utilizando o XML dentro do TypeScript}
+//Componente é uma função que retorna HTML {Sempre ter a letra inicial Maiuscula}
+export default App
